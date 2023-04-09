d531fe2ec15776869c517c39a90c09f9dc797f3e
Fotall <d.budarin@list.ru>
2023-04-09 17:39:10 +0300

    save2

--- homeWork_01.py
+++ homeWork_01.py
@@ -4,18 +4,32 @@
 print('Введите размеры шоколадки ')
 chocolateN = int(input('по горизонтали:  '))
 chocolateM = int(input('по вертикали:  '))
-deliteCocolate = int(input('сколько долек надо отломать:  '))
+deliteCocolate = int(input('сколько долек надо отломать(от 1 до 4):  '))
+if (deliteCocolate == 2):
+    if (chocolateN == 2 and chocolateM > 1): 
+        print('yes')
+    elif (chocolateN == 1 and chocolateM % 2 == 0):
+        print('yes')
+    else:
+        print('no')
+elif (deliteCocolate == 3):
+    if (chocolateN == 1 and chocolateM % 2 == 0):
+        print('yes')
+    else:
+        print('no')
+elif (deliteCocolate == 4):
+    if (chocolateN == 2 and chocolateM > 1): 
+        print('yes')
+    elif  (chocolateN == 1 and chocolateM % 4 == 0): 
+        print('yes')
+    else:
+        print('no')
+elif (deliteCocolate == 1):
+    if (chocolateN == 1):
+        print('yes')
+    else:
+        print('no')
+else:
+    print('программа пока написана для вычесления от 1 до 4')
 
-
-
-
-
-
-
-# if (chocolateN == 2 and chocolateM > 1): 
-#     print('yes')
-# elif  (chocolateN == 1 and chocolateM % 4== 0): 
-#     print('yes')
-# else:
-#     print('no')
 
